13800fb3583763a87d6cd900ba5f7c3029139be8
briandangerflynn <bdflynny@gmail.com>
2019-03-28 17:12:28 -0400

    all the stuff

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(user_id: @user.id)
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to user_post_path(@user, @post)
+    end
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to user_post_path(@user, @post)
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @post.destroy
+    redirect_to user_posts_path(@user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
+end
